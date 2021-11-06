75e270a159d15f5356a931f2836bed22ae98b3fc
Avoz194 <63339876+Avoz194@users.noreply.github.com>
2021-11-06 17:57:21 +0200

    change of structure

--- apiActions.go
+++ apiActions.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	ent "github.com/Avoz194/goGo/Entities"
+	"math/rand"
+	"strconv"
+)
+
+func createPerson(name, email string) ent.Person {
+	return ent.Person{Id: strconv.Itoa(rand.Intn(10000)), Name: name, Email: email, Tasks: nil}
+}
+
+func addPerson(person ent.Person) {
+	persons = append(persons, person)
+}
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
-
 	ent "github.com/Avoz194/goGo/entities"
 )
 
 var persons = []ent.Person{}
+var tasks = []ent.Task{}
 
 func main() {
 	p1 := createPerson("Gilsss", "ss")
@@ -19,13 +17,6 @@ func main() {
 	fmt.Println(persons)
 }
 
-func createPerson(name, email string) ent.Person {
-	return ent.Person{Id: strconv.Itoa(rand.Intn(10000)), Name: name, Email: email, Tasks: nil}
-}
-
-func addPerson(person ent.Person) {
-	persons = append(persons, person)
-}
 
 // func removePerson(){
 // 	persons=
