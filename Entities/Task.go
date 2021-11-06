041304bf98cafa5b867a37db5a605a3a601770fb
Avoz194 <63339876+Avoz194@users.noreply.github.com>
2021-11-06 10:30:48 +0200

    tmp commit
    move project to different directory

--- Entities/Task.go
+++ Entities/Task.go
@@ -6,20 +6,29 @@ import (
 
 type Task struct {
 	id      string
+	title   string
 	ownerId string
 	details string
 	status  Status
 	dueDate time.Time
 }
 
-func (t *Task) setOwner(id string) {
-	t.ownerId = id
+func (t *Task) setOwner(newOwnerid string) {
+	t.ownerId = newOwnerid
 }
 
 func (t *Task) setStatus(newStatus Status) {
 	t.status = newStatus
 }
 
-func (t *Task) isDone() {
+func (t *Task) isDone() bool {
 	return t.status.isDone()
+}
+
+func (t *Task) getStatus() Status {
+	return t.status
+}
+
+func (t *Task) getOwnerID() string {
+	return t.ownerId
 }
